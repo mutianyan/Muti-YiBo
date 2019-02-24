8e0e91cce14b53615a997e8f8ee40db579ab707f
chenyinbo <chenyinbo@chenyinbos-MacBook-Pro.local>
2019-02-24 02:59:43 -0700

    added shortcuts keys controller which can be controlled through bluetooth device. Added images import function from the local folds.

--- picture.js
+++ picture.js
@@ -1,4 +1,72 @@
 $(document).ready(function() { 
+    i=1;
+    var flag1 = 0;
+    var flag2 = 0;
+    
+    $("#controlbuttonright").click(function(){
+        changeImgAdd();
+    });
+    $("#controlbuttonleft").click(function(){
+        changeImgSubst();
+    });
+    
+    function changeImgAdd(){
+        i++;
+        document.getElementById("imagchange").src="images/imag"+i+".jpg";
+        return i; 
+    }
+    function changeImgSubst(){
+        i--;
+        document.getElementById("imagchange").src="images/imag"+i+".jpg";
+        return i; 
+    }
+    
+    
+    function ergodic(str){
+        
+        
+        if(flag1<1&&flag2<1){
+            //alert("x!=1");
+            for(j=1;j<20;j++){
+            var bigImg= document.createElement("img");
+            bigImg.src ="images/imag"+j+".jpg";
+            bigImg.width="200";
+            bigImg.height="200";
+            var myp = document.getElementById(str);
+            myp.appendChild(bigImg);
+            };
+            flag1++;
+        }else if(flag1<1||flag2<1){
+            for(j=1;j<20;j++){
+                var bigImg= document.createElement("img");
+                bigImg.src ="images/imag"+j+".jpg";
+                bigImg.width="200";
+                bigImg.height="200";
+                var myp = document.getElementById(str);
+                myp.appendChild(bigImg);
+                };
+                flag2++;
+        }else{
+            return;
+        }
+            
+            
+        
+    }
+    
+    document.onkeydown= function (e) {
+        var theEvent = window.event || e; 
+        var code = theEvent.keyCode || theEvent.which; 
+        if (code == 37) { 
+            $("#controlbuttonleft").click(); 
+        }else if(code == 39){
+            $("#controlbuttonright").click();
+        }
+        
+        }
+
+    
+
     $("#tab_bar").show();
     //$("#tab_bar").addClass("end");
     //$("#DialerUp").addClass("end");
@@ -25,6 +93,7 @@ $(document).ready(function() {
     $("#imageView3").hide(); 
 });      
     $("#secondMethod").click(function() {
+         
     // if($(this).hasClass("end")){
     //     $(this).removeClass("end");
     //     $(this).addClass("start");
@@ -36,9 +105,11 @@ $(document).ready(function() {
     //     };   
    
     $("#tab_bar").show(); 
+    ergodic("imageshow2");
     $("#imageView2").show();
     $("#imageView1").hide(); 
     $("#imageView3").hide();
+    
 }); 
     $("#thirdMethod").click(function() { 
     // if($(this).hasClass("end")){
@@ -50,8 +121,9 @@ $(document).ready(function() {
     //     $("#ContactListUp").addClass("end");
     //     $("#DialerUp").addClass("end");   
     //     };     
-     
+     a=0;
     $("#tab_bar").show(); 
+    ergodic("unit");
     $("#imageView3").show();
     $("#imageView1").hide(); 
     $("#imageView2").hide();
