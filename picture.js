f65e8775b533b00442451e32d189532a0bdf563a
mutianyan <mutianyan@rgnt2-125-76-dhcp.int.colorado.edu>
2019-02-26 18:36:31 -0700

    try again

--- picture.js
+++ picture.js
@@ -1,72 +1,4 @@
 $(document).ready(function() { 
-    i=1;
-    var flag1 = 0;
-    var flag2 = 0;
-    
-    $("#controlbuttonright").click(function(){
-        changeImgAdd();
-    });
-    $("#controlbuttonleft").click(function(){
-        changeImgSubst();
-    });
-    
-    function changeImgAdd(){
-        i++;
-        document.getElementById("imagchange").src="images/imag"+i+".jpg";
-        return i; 
-    }
-    function changeImgSubst(){
-        i--;
-        document.getElementById("imagchange").src="images/imag"+i+".jpg";
-        return i; 
-    }
-    
-    
-    function ergodic(str){
-        
-        
-        if(flag1<1&&flag2<1){
-            //alert("x!=1");
-            for(j=1;j<20;j++){
-            var bigImg= document.createElement("img");
-            bigImg.src ="images/imag"+j+".jpg";
-            bigImg.width="200";
-            bigImg.height="200";
-            var myp = document.getElementById(str);
-            myp.appendChild(bigImg);
-            };
-            flag1++;
-        }else if(flag1<1||flag2<1){
-            for(j=1;j<20;j++){
-                var bigImg= document.createElement("img");
-                bigImg.src ="images/imag"+j+".jpg";
-                bigImg.width="200";
-                bigImg.height="200";
-                var myp = document.getElementById(str);
-                myp.appendChild(bigImg);
-                };
-                flag2++;
-        }else{
-            return;
-        }
-            
-            
-        
-    }
-    
-    document.onkeydown= function (e) {
-        var theEvent = window.event || e; 
-        var code = theEvent.keyCode || theEvent.which; 
-        if (code == 37) { 
-            $("#controlbuttonleft").click(); 
-        }else if(code == 39){
-            $("#controlbuttonright").click();
-        }
-        
-        }
-
-    
-
     $("#tab_bar").show();
     $("#imageView1").show(); 
     $("#imageView2").hide();
@@ -78,47 +10,16 @@ $(document).ready(function() {
     $("#imageView2").hide();
     $("#imageView3").hide(); 
 });      
-<<<<<<< Updated upstream
-    $("#secondMethod").click(function() {
-         
-    // if($(this).hasClass("end")){
-    //     $(this).removeClass("end");
-    //     $(this).addClass("start");
-    //     $("#DialerUp").addClass("end");
-    //     $("#AddContactUp").addClass("end");
-    // }else{
-    //     $("#DialerUp").addClass("end");
-    //     $("#AddContactUp").addClass("end");
-    //     };   
-   
-=======
     $("#secondMethod").click(function() { 
->>>>>>> Stashed changes
     $("#tab_bar").show(); 
-    ergodic("imageshow2");
     $("#imageView2").show();
     $("#imageView1").hide(); 
     $("#imageView3").hide();
-    
 }); 
     $("#thirdMethod").click(function() { 
-<<<<<<< Updated upstream
-    // if($(this).hasClass("end")){
-    //     $(this).removeClass("end");
-    //     $(this).addClass("start");
-    //     $("#ContactListUp").addClass("end");
-    //     $("#DialerUp").addClass("end");
-    // }else{
-    //     $("#ContactListUp").addClass("end");
-    //     $("#DialerUp").addClass("end");   
-    //     };     
-     a=0;
-=======
   
      
->>>>>>> Stashed changes
     $("#tab_bar").show(); 
-    ergodic("unit");
     $("#imageView3").show();
     $("#imageView1").hide(); 
     $("#imageView2").hide();
